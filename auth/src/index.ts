61f36019bed5c06ba927117d570853f8b6b07a65
Qasim Hafeez <qasimh556@gmail.com>
2020-09-14 11:04:32 +0400

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { json } from "body-parser";
+
+const app = express();
+app.use(json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
