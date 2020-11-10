e2448ec2d0c9697d068e449baf55e682ddbc7f6f
Qasim Hafeez <qasimh556@gmail.com>
2020-11-10 16:47:07 +0400

    added error handling middle ware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
+import {errorHandler} from "./middleware/error-handler";
 
 const app = express();
 app.use(json());
@@ -12,6 +13,8 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(3000, () => {
