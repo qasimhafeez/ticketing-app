e91cd4584c63c51b2a022a1ee18ef11f3016058c
Qasim Hafeez <qasimh556@gmail.com>
2020-11-11 17:46:45 +0400

    mongo db configration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
@@ -22,6 +24,19 @@ app.all("*", async () => {
 });
 
 app.use(errorHandler);
+
+const connect = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Mongodb Connected");
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const PORT = 3000;
 app.listen(3000, () => {
