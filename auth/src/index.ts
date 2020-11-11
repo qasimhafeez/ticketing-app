cd718831d849dde15f4824000257d3c29770c941
Qasim Hafeez <qasimh556@gmail.com>
2020-11-11 16:38:39 +0400

    async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
@@ -16,7 +17,7 @@ app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 
-app.all("*", () => {
+app.all("*", async () => {
   throw new NotFoundError();
 });
 
