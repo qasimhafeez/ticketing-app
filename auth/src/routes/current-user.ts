3c1a6f2e217cbd1c886dff45c6d0aab902a8b26e
Qasim Hafeez <qasimh556@gmail.com>
2020-11-14 16:47:01 +0400

    refactoring

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+// Importing middleware
+import { currentUser } from "../middleware/current-user";
 
 const router = express();
 
-router.get("/api/users/currentUser", (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  "/api/users/currentUser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
