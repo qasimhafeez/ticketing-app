68103c554e454254cf67fe766e312dc7dbceb22c
Qasim Hafeez <qasimh556@gmail.com>
2020-11-14 16:16:33 +0400

    returning the current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const router = express();
 
-router.get("/api/users/currentUser", (req, res) => {
-  res.send("Working");
+router.get("/api/users/currentUser", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
