8000a033540370d376c37343cdf5e589efe19ee8
Qasim Hafeez <qasimh556@gmail.com>
2020-11-14 16:24:43 +0400

    sign in complete

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middleware/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = express();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Working");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must enter your password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Your email or password is wrong");
+    }
+
+    const passwordsMatch = Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError("Your password is not correct.");
+    }
+
+    // Generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it in session
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
