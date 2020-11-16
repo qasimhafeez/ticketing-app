15251dcd16277757caf389333dd08e4bc01f73a2
Qasim Hafeez <qasimh556@gmail.com>
2020-11-16 12:49:02 +0400

    sign in tests complete

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,46 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails if email does not exist", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("fails on incorrect password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "wrongpassword",
+    })
+    .expect(200);
+});
+
+it("response with cookie with valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
