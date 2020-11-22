3b13f0d1446600527b3830c66364fd224a595c9d
Qasim Hafeez <qasimh556@gmail.com>
2020-11-22 21:09:01 +0400

    removed typos

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
     return axios.create({
       baseURL: "http://ingress-nginx.ingress-nginx.svc.cluster.local",
       headers: req.headers,
