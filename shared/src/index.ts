e5228f07339f9a944b6f69c6c94e7014833ce1b6
Qasim Hafeez <qasimh556@gmail.com>
2020-11-25 10:24:01 +0400

    updates

---
+++ shared/src/index.ts
@@ -0,0 +1,13 @@
+// Exporting errors
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
+
+// Exporting middleware
+export * from "./middleware/current-user";
+export * from "./middleware/error-handler";
+export * from "./middleware/require-auth";
+export * from "./middleware/validate-request";
