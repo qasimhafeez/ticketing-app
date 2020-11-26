f8b40e38a97a2f5516a9ba4a037c29800e106760
Qasim Hafeez <qasimh556@gmail.com>
2020-11-26 12:04:04 +0400

    new ticket created test completed

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,76 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("/api/tickets for post request", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.status).not.toEqual(404);
+});
+
+it("only access it if use is logged in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
+});
+
+it("return a status other than 401 if user is signed in", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signup())
+    .send({});
+  expect(response.status).not.toEqual(401);
+});
+
+it("error if invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signup())
+    .send({
+      title: "",
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signup())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+
+it("error if invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signup())
+    .send({
+      title: "new title",
+      price: -20,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signup())
+    .send({
+      title: "new title",
+    })
+    .expect(400);
+});
+
+it("check if ticket has valid inputs", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signup())
+    .send({
+      title: "new title",
+      price: 10,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(10);
+});
