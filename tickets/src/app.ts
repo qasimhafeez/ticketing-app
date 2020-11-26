979756d1a55bb4b40e33a87107e6795a5c8a55f2
Qasim Hafeez <qasimh556@gmail.com>
2020-11-26 12:06:21 +0400

    updated

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,9 +2,14 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-
 // Handling Errors
-import { errorHandler, NotFoundError } from "@qh-tickets/shared";
+import { errorHandler, NotFoundError, currentUser } from "@qh-tickets/shared";
+
+// Routes
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -16,6 +21,12 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
