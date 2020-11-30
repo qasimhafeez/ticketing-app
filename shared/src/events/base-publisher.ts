14b975936536f23020dda3adde0f2153ab2ea489
Qasim Hafeez <qasimh556@gmail.com>
2020-12-01 01:01:05 +0400

    updates

--- shared/src/events/base-publisher.ts
+++ shared/src/events/base-publisher.ts
@@ -15,7 +15,7 @@ export abstract class Publisher<T extends Event> {
     this.client = client;
   }
 
-  publish(data: T["data"]) {
+  publish(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err) => {
         if (err) {
