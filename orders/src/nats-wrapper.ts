c52bfe0dfd70e82f7dc38ccc10a4d98fee5ecdda
Qasim Hafeez <qasimh556@gmail.com>
2020-12-03 19:59:25 +0400

    initial commit

---
+++ orders/src/nats-wrapper.ts
@@ -0,0 +1,42 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+  // Might be undefined for some period of time
+  private _client?: Stan;
+
+  // getter
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    this._client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => {
+      this.client.close();
+    });
+    process.on("SIGTERM", () => {
+      this.client.close();
+    });
+
+    return new Promise((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this.client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
