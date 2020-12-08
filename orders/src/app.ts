6551404080df4c1b25991fdd2bd28e87f4c37894
Qasim Hafeez <qasimh556@gmail.com>
2020-12-08 21:20:22 +0400

    removed errors and routes imported

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -6,6 +6,10 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@qh-tickets/shared";
 
 // Routes
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes/index";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -19,6 +23,11 @@ app.use(
 );
 
 app.use(currentUser);
+
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
