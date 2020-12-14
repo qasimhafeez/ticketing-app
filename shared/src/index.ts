7283231e91c40edc50d7d6bea840397f96ec8f3c
Qasim Hafeez <qasimh556@gmail.com>
2020-12-14 17:21:07 +0400

    updates

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -18,3 +18,6 @@ export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-created-event";
 export * from "./events/ticket-updated-event";
+
+// Exporting event types
+export * from "./events/types/order-status";
