6c0c4b03dbc52906ddbb4873cbec552b4ab521a5
Qasim Hafeez <qasimh556@gmail.com>
2021-01-02 17:36:40 +0400

    ticket model

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,7 +1,45 @@
 import mongoose from "mongoose";
 
-interface TicketAttrs {}
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
 
-interface TicketDoc extends mongoose.Document {}
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
 
-interface TicketModel extends mongoose.Model<TicketDoc> {}
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
