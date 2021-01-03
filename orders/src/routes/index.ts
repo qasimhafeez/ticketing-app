8bb0c7f181a62a8f2c60cbe1bce0c4401c726868
Qasim Hafeez <qasimh556@gmail.com>
2021-01-03 11:29:04 +0400

    get all the order and tickets associated with it

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
+import { requireAuth } from "@qh-tickets/shared";
+import { Order } from "../models/order";
+import { Ticket } from "../models/ticket";
+
 const router = express.Router();
 
-router.get("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket");
+  res.send({ orders });
 });
 
 export { router as indexOrderRouter };
