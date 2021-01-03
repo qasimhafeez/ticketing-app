0344f37c39a5aca53cb3c801f8cd3b47a8718e30
Qasim Hafeez <qasimh556@gmail.com>
2021-01-03 10:42:47 +0400

    new order api

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import { requireAuth, validateRequest } from "@qh-tickets/shared";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@qh-tickets/shared";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
 import { Order } from "../models/order";
 
 const router = express.Router();
+const EXPIRATION_TIME = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -20,8 +27,31 @@ router.post(
   validateRequest,
   async (req: Request, res: Response) => {
     // Find the ticker user is trying to buy in the database
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
 
-    res.send({});
+    // Make sure that ticket is not reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // Calculate and expiration date for the order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_TIME);
+
+    // Build and save
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    res.status(201).send(order);
   }
 );
 
