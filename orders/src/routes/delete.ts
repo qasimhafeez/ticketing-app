2115326f8769a7abb54d3937477eccc77cba3881
Qasim Hafeez <qasimh556@gmail.com>
2021-01-05 15:57:27 +0400

    deleted route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,8 +1,34 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@qh-tickets/shared";
+import { Order, OrderStatus } from "../models/order";
+import { body } from "express-validator";
+
 const router = express.Router();
 
-router.delete("/api/orders/:id", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
