3bee49961bb0fca5efba365408f40d2e62b853c3
Qasim Hafeez <qasimh556@gmail.com>
2021-01-05 12:44:26 +0400

    index test case

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,55 @@
+import request from "supertest";
+import { isConstructorDeclaration } from "typescript";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "ticket",
+    price: 10,
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+it("fetches order of a particular user", async () => {
+  // create 3 tickets and save them to db
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signup();
+  const userTwo = global.signup();
+
+  // create 1 order as user#1
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // create 2 orders as user#2
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  // make request to get orders from user#2
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  // make sure we only get orders for user#2
+  console.log(response.body);
+  // expect(response.body.length).toEqual(2);
+});
