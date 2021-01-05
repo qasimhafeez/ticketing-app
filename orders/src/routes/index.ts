e9f251baf99f925c12b7383d5cd777abae1db22d
Qasim Hafeez <qasimh556@gmail.com>
2021-01-05 12:44:42 +0400

    show orders api

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { requireAuth } from "@qh-tickets/shared";
 import { Order } from "../models/order";
-import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -9,7 +8,8 @@ router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
   const orders = await Order.find({
     userId: req.currentUser!.id,
   }).populate("ticket");
-  res.send({ orders });
+
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
